a9295b4299211b9cf0285c96f23e677a6a045c20
olgamandel <olhamandel@gmail.com>
2023-05-24 21:19:08 +0200

    ilimitadoClasificacionIniciarSesion

--- Frontend/MovieGuesser/src/app/app-routing.module.ts
+++ Frontend/MovieGuesser/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { DiarioComponent } from './pagina/diario/diario.component';
 import { IlimitadoComponent } from './pagina/ilimitado/ilimitado.component';
 import { ClasificacionComponent } from './pagina/clasificacion/clasificacion.component';
 import { Error404Component } from './error404/error404.component';
+import { LoginComponent } from './pagina/login/login.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
 
 const routes: Routes = [
 {path:'diario', component:DiarioComponent},
 {path:'ilimitado', component:IlimitadoComponent},
 {path: 'clasificacion', component: ClasificacionComponent },
+{path: 'login', component: LoginComponent },
+{path: 'registro', component: RegistroComponent },
 {path: '', redirectTo: '/diario', pathMatch: 'full'},
 {path: '**', component: Error404Component }
 ];
